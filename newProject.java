30d12c94fc161e63edf8dcb97442b1ad95a83528
JP Vernon <jp.vernon3@gmail.com>
2025-04-24 20:59:51 -0600

    random

---
+++ newProject.java
@@ -0,0 +1,27 @@
+public class newProject {
+	public static void main(String[] args) {
+		String dog = "yes";
+		boolean cat = true;
+		boolean partner = true;
+		int pets = 3;
+		int partners = 1;
+		String home = "Colorado";
+		String hobby = "hiking";
+
+		System.out.println("Do you have a dog?");
+		System.out.println(dog);
+		System.out.println("Do cats rule?");
+		System.out.println(cat);
+		System.out.println("Is your girlfriend cool?");
+		System.out.println(partner);
+		System.out.println("Where do you call home?");
+		System.out.println(home);
+		System.out.println("Whats's your favorite hobby?");
+		System.out.println(hobby);
+		System.out.println("how many pets do you have?");
+		System.out.println(pets);
+		System.out.println("how many partners does a man need?");
+		System.out.println(partners);
+		
+	}	
+}
